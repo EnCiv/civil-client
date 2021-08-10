fdea145f235aa98080cea86c2e25681394e3cb5a
David Fridley <ddfridley@yahoo.com>
2021-08-10 13:10:16 -0700

    ErrorBoundry too

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 
 import AuthForm from './components/auth-form/index.jsx'
 import clientMain from './client/main.js'
+import ErrorBoundry from './components/error-boundary'
 
 // do NOT try to pass browser/client side objects through here (like AuthForm) - when you import them it will also import the server into the browser
-export { AuthForm, clientMain }
+export { AuthForm, clientMain, ErrorBoundry }
