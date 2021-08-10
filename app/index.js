6b8da699ec7710821fadaa771255a158126e9a75
David Fridley <ddfridley@yahoo.com>
2021-08-10 13:16:27 -0700

    fix authform .jsx

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-import AuthForm from './components/auth-form/index.jsx'
-import clientMain from './client/main.js'
+import AuthForm from './components/auth-form'
+import clientMain from './client/main'
 import ErrorBoundry from './components/error-boundary'
 
 // do NOT try to pass browser/client side objects through here (like AuthForm) - when you import them it will also import the server into the browser
